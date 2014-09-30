f358e48cdaf3a1288d25713f70de18561e6ba3a5
Charles Bonnissent <charles.bonnissent@anakeen.com>
2014-09-30 15:39:47 +0200

    add wfl

---
+++ COGIP_AUDIT/COGIP_AUDIT_AUDIT__WFL.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace COGIP;
+
+class COGIP_AUDIT_AUDIT__WFL extends \Dcp\Family\WDoc
+{
+    public $attrPrefix = 'caaw';
+    public $firstState = self::e_brouillon;
+    public $viewlist = "none";
+
+    //region MyAttributes-constants
+    //endregion
+
+    //region States
+    /* @stateLabel _("coa_e1") */
+    const e_brouillon = 'coa_e1';
+    /* @stateLabel _("coa_e2") */
+    const e_annule = 'coa_e2';
+    /* @stateLabel _("coa_e3") */
+    const e_redaction = 'coa_e3';
+    /* @stateLabel _("coa_e4") */
+    const e_certifie = 'coa_e4';
+    /* @stateLabel _("coa_e5") */
+    const e_refus_certif = 'coa_e5';
+    //endregion
+
+    //region Transitions
+    /* @stateLabel _("coa_t1") */
+    const t_brouillon__redaction = 'coa_t1';
+    /* @stateLabel _("coa_t2") */
+    const t_brouillon__annule = 'coa_t2';
+    /* @stateLabel _("coa_t3") */
+    const t_redaction__brouillon = 'coa_t3';
+    /* @stateLabel _("coa_t4") */
+    const t_redaction__certif = 'coa_t4';
+    /* @stateLabel _("coa_t5") */
+    const t_redaction__refus_certif = 'coa_t5';
+    //endregion
+
+    public $stateactivity = array(
+        /* @stateLabel _("coa_planning") */
+        self::e_brouillon => "coa_planning",
+        /* @stateLabel _("coa_writing") */
+        self::e_redaction => "coa_writing"
+    );
+
+    public $transitions = array(
+        self::t_brouillon__redaction => array("nr" => true),
+        self::t_brouillon__annule => array("nr" => true),
+        self::t_redaction__brouillon => array("nr" => true),
+        self::t_redaction__certif => array("nr" => true),
+        self::t_redaction__refus_certif => array("nr" => true),
+    );
+
+    public $cycle = array(
+        array("t" => self::t_brouillon__redaction, "e1" => self::e_brouillon, "e2" => self::e_redaction),
+        array("t" => self::t_brouillon__annule, "e1" => self::e_brouillon, "e2" => self::e_annule),
+        array("t" => self::t_redaction__brouillon, "e1" => self::e_redaction, "e2" => self::e_brouillon),
+        array("t" => self::t_redaction__certif, "e1" => self::e_redaction, "e2" => self::e_certifie),
+        array("t" => self::t_redaction__refus_certif, "e1" => self::e_redaction, "e2" => self::e_refus_certif),
+    );
+
+}
