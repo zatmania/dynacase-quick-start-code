1bb3698b9790a23dfca18661e8bc1dd1a9a4e855
Charles Bonnissent <charles.bonnissent@anakeen.com>
2014-09-30 12:38:36 +0200

    add hooks (3.2-after-30-40)

--- COGIP_AUDIT/COGIP_AUDIT_FNC__CLASS.php
+++ COGIP_AUDIT/COGIP_AUDIT_FNC__CLASS.php
@@ -7,6 +7,20 @@ use \Dcp\AttributeIdentifiers\COGIP_AUDIT_FNC as MyAttributes;
 Class COGIP_AUDIT_FNC extends \Dcp\Family\COGIP_AUDIT_BASE {
 
     /**
+     * Hook executed after the store
+     *
+     * @return string|void
+     */
+    public function postStore()
+    {
+        $err = parent::postStore();
+        $err .= $this->refreshAudit();
+        if ($err) {
+            error_log(__FILE__ . ":" . __LINE__ . ":" . __METHOD__ . " " . $err . "\n");
+        }
+    }
+
+    /**
      * Compute the title of the FNC family
      *
      * @return string
@@ -19,6 +33,25 @@ Class COGIP_AUDIT_FNC extends \Dcp\Family\COGIP_AUDIT_BASE {
         $chapterTitle = $this->getTitle($this->getAttributeValue(MyAttributes::caf_audit));
         // Compose the title
         return sprintf("%s : %s", $chapterTitle, $title);
+    }
+
+    /**
+     * -   Refresh the list of the audit
+     *
+     * @return string
+     */
+    public function refreshAudit()
+    {
+        $err = "";
+        $audit = $this->getAttributeValue(MyAttributes::caf_audit);
+        $audit = new_Doc("", $audit, true);
+        /* @var \Dcp\Family\COGIP_AUDIT_AUDIT $audit */
+        $err .= $audit->computeFNC();
+        $err .= $audit->store();
+        if ($err) {
+            $err = __FILE__ . ":" . __LINE__ . ":" . __METHOD__ . " " . $err . "\n";
+        }
+        return $err;
     }
 
 }
